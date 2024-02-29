b3d0416cbf269a8cbd5a0f38b91cf6db6e214aa7
navjeet1999 <108082679+navjeet1999@users.noreply.github.com>
2024-02-29 18:17:06 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "proj-rt" {
 resource "aws_subnet" "proj-subnet" {
  vpc_id = aws_vpc.proj-vpc.id
  cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1"
+ availability_zone = "ap-south-1a"
  tags = {
  Name = "subnet1"
  }
@@ -128,7 +128,7 @@ resource "aws_eip" "proj-eip" {
 resource "aws_instance" "Prod-Server" {
  ami = "ami-0c7217cdde317cfec"
  instance_type = "t2.micro"
- availability_zone = "ap-south-1a"
+ availability_zone = "ap-south-1"
  key_name = "mynewkey"
  network_interface {
  device_index = 0
