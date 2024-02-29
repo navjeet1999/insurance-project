e62a9570fbc2e8b28166aeb165735638bc2bd069
navjeet1999 <108082679+navjeet1999@users.noreply.github.com>
2024-02-29 17:24:30 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -128,7 +128,7 @@ resource "aws_eip" "proj-eip" {
 resource "aws_instance" "Prod-Server" {
  ami = "ami-0c7217cdde317cfec"
  instance_type = "t2.micro"
- availability_zone = "aus-east-1a"
+ availability_zone = "us-east-1a"
  key_name = "mynewkey"
  network_interface {
  device_index = 0
